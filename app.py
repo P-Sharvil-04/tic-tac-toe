ee3b8e23912f61831f79664d6312a465ac152bf9
sharvil <sharvil.tmbs25@gmail.com>
2026-04-24 15:47:14 +0530

    render fix

--- app.py
+++ app.py
@@ -117,3 +117,9 @@ def reset(data):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host="0.0.0.0", port=port)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
